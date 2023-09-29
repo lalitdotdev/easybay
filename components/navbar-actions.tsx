b7128777d18c2fdd15784415ba2fd6a8460063dd
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-29 16:43:13 +0530

    Conditional rendering navactions to prevent hydration error

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -2,8 +2,15 @@
 
 import { ShoppingBag } from "lucide-react";
 import Button from "./ui/button";
+import { use, useEffect, useState } from "react";
 
 const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) return null;
+
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
