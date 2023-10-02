c8cc269d028a2a9d61aa4259584c7125c34b6c42
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 17:47:12 +0530

    Created a ProductCard component for displaying product details, including images, name, category, and price. Users can click to view more, preview the product, and add it to the cart. It's designed for product listings with a clean, responsive layout.

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,15 +6,30 @@ import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
   data: Product;
 }
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
-  const router = useRouter();
+  const cart = useCart();
 
+  const router = useRouter();
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const previewModal = usePreviewModal();
+  const onPreview: MouseEventHandler<HTMLButtonElement> = e => {
+    e.stopPropagation();
+    previewModal.onOpen(data);
+  };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = e => {
+    e.stopPropagation();
+    cart.addItem(data);
   };
 
   return (
@@ -33,11 +48,11 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-4 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
