96cffdc1c36f4b9ee8ccbb9c848d4002ad725bfc
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 17:27:42 +0530

    Implement ModalProvider for managing modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import Footer from "@/components/footer";
-import Navbar from "@/components/navbar";
-import ToastProvider from "@/providers/toast-provider";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
-import "./globals.css";
+import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <ToastProvider />
         <Navbar />
         {children}
