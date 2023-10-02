d852402b47d1b31ca02d6aa72afb64b316169481
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 17:26:44 +0530

    Implement ToastProvider for displaying notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import Footer from "@/components/footer";
-import "./globals.css";
+import Navbar from "@/components/navbar";
+import ToastProvider from "@/providers/toast-provider";
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
-import Navbar from "@/components/navbar";
+import "./globals.css";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
