b29cd8eafa3924b0baf1e6e1a33370438c392828
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 17:38:20 +0530

    Implement shopping cart icon in NavbarActions to navigate to cart page

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -3,19 +3,28 @@
 import { ShoppingBag } from "lucide-react";
 import Button from "./ui/button";
 import { use, useEffect, useState } from "react";
+import useCart from "@/hooks/use-cart";
+import { useRouter } from "next/navigation";
 
 const NavbarActions = () => {
+  const router = useRouter();
   const [isMounted, setIsMounted] = useState(false);
   useEffect(() => {
     setIsMounted(true);
   }, []);
+  const cart = useCart();
   if (!isMounted) return null;
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">Cart</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
