6a115097cd6e986b29e1d1c4f8e80d09fd39dfad
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 17:52:00 +0530

    Created a Footer component that displays the copyright information at the bottom of the page. The copyright year is dynamically generated based on the current year.

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; {new Date().getFullYear()} Store FakeStoreNameA, Inc. All
+          rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
