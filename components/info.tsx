8eedb9797218eb310346c9243fe2e9f0840d8bef
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-08 19:09:11 +0530

    Fix: AddtoCart not working on individual product page ,now added onAddtoCart handler utilising the useCart Custom hook for handling cart related tohandling cart state

--- components/info.tsx
+++ components/info.tsx
@@ -1,15 +1,22 @@
-"use client";
+'use client';
 
-import { Product } from "@/types";
-import Currency from "./ui/currency";
-import Button from "./ui/button";
-import { ShoppingCart } from "lucide-react";
+import { Product } from '@/types';
+import Currency from './ui/currency';
+import Button from './ui/button';
+import { ShoppingCart } from 'lucide-react';
+import { MouseEventHandler } from 'react';
+import useCart from '@/hooks/use-cart';
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -27,14 +34,11 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
-          <div
-            className="h-6 w-6 rounded-full border border-gray-600"
-            style={{ backgroundColor: data.color.value }}
-          />
+          <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data.color.value }} />
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button className="flex items-center gap-x-2" onClick={onAddToCart}>
           Add to cart <ShoppingCart />
         </Button>
       </div>
